4e07f72a9a79e83a3539cff279eb52d733208d4c
larmen9179 <lo.j.armendariz@gmail.com>
2023-10-05 21:37:13 -0600

    completed init/add history

--- src/history.c
+++ src/history.c
@@ -1,8 +1,10 @@
 #ifndef _HISTORY_
 #define _HISTORY_
 
-#include "history.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
 
 typedef struct s_Item {
   int id;
@@ -15,14 +17,70 @@ typedef struct s_List {
 } List;
 
 /* Initialize the linked list to keep the history. */
-List* init_history();
+List* init_history(){
 
+  List *tokenHist = malloc(sizeof(List));
+
+  tokenHist->root = NULL;
+
+  return tokenHist;
+  
+}
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
+void add_history(List *list, char *str){
 
+  /*Finding the length by iterating*/
+  int tokenLen;
+  
+  for(tokenLen = 0;str[i] != '\0';i += 1){
+    ;
+  }
+
+  char *newItemPtr = copy_str(str, tokenLen);
+
+  int nodeId = 0;
+
+  Item *currentItem = list -> root;
+
+  /*Checking if the root is empty*/
+  if(currentItem == NULL){
+
+    currentItem = malloc(sizeof(Item));
+    
+    currentItem->id = 0;
+    
+    currentItem->str = newItemPtr;
+
+    currentItem->next = NULL;
+
+    list->root = currentItem;
+
+    return;
+  }
+
+  /*Iterating if the root isn't empty*/
+  while(currentItem->next != '\0'){
+    
+    nodeId += 1;
+
+    /*??????????*/
+    currentItem ->= next;
+  }
+
+  /*Allocating a new node once we've reached the end*/
+
+  currentItem->next = malloc(sizeof(Item));
+
+  currentItem->next->id = nodeId;
+
+  currentItem->next->str = newItemPtr;
+
+  currentItem->next->next = '\0';
+  
+}
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
