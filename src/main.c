d305d9c1096d94fc02f87651e7fcd4c9ea782f21
larmen9179 <lo.j.armendariz@gmail.com>
2023-10-17 22:23:34 -0600

    added terminal input for tokenizer

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 
 main(){
 
+  char option[3];
+  char stringUser[100];
+    
   List *history = init_history();
 
   while(1){
@@ -12,8 +15,42 @@ main(){
     printf("Welcome to the tokenizer...");
     putchar('\n');
 
-    printf("Here is the options menu, please input your number");
+    printf("Here is the options menu, please input your selection...");
     putchar('\n');
+
+    printf("w: Enter a string\n");
+    printf("!h: See history\n");
+    printf("!i: Use the '!' symbol and a number instead of i to see a specific history\n");
+
+    putchar('>');
+
+    fgets(option, 3, stdin);
+    
+    if(*option == 'w'){
+      
+      printf("Enter in your string: \n");
+      putchar('>');
+      fgets(stringUser, 100, stdin);
+
+      char **tokenizer = tokenize(stringUser);
+
+      print_tokens(tokenizer);
+
+      add_history(history, stringUser);
+    }
+
+    else if(option[0] == '!'){
+      if(option[1] == 'h'){
+
+	print_history(history);
+	
+      }
+    }
+    else{
+      printf("Your selection was invalid, goodbye!!\n");
+      break;
+    }
   }
-  
+  free_history(history);
+  return 0;
 }
